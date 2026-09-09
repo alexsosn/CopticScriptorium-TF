0492bac129a93936d8d34a3cdd3e1467c339b596
Oleksandr (Alex) Sosnovshchenko <alex.sosn88@gmail.com>
2026-09-09 13:36:53 +0300

    test: cover Pepper PAULA metadata base

--- tests/test_issue1_paula_audit.py
+++ tests/test_issue1_paula_audit.py
@@ -1,5 +1,4 @@
 import importlib.util
-import json
 from pathlib import Path
 import tempfile
 import unittest
@@ -38,6 +37,12 @@ META_XML = '''<?xml version="1.0"?>
 <paula version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
 <header paula_id="demo.meta_license"/>
 <featList type="license" xml:base="demo.anno.xml"><feat xlink:href="#anno_1" value="CC-BY 4.0"/></featList>
+</paula>
+'''
+PEPPER_META_XML = '''<?xml version="1.0"?>
+<paula version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink">
+<header paula_id="demo.meta_author"/>
+<featList type="author" xml:base="meta"><feat xlink:href="#anno_1" value="Demo Author"/></featList>
 </paula>
 '''
 ANNOFEAT_XML = '''<?xml version="1.0"?>
@@ -87,115 +92,57 @@ class PaulaAuditContractTests(unittest.TestCase):
     def test_inventories_paula_xml_kinds_and_metadata_features(self):
         with tempfile.TemporaryDirectory() as tmp:
             root = Path(tmp)
-            self._write_archive(
-                root,
-                "demo",
-                "demo",
-                {
-                    "demo/doc/doc.text.xml": TEXT_XML,
-                    "demo/doc/doc.tok.xml": TOK_XML,
-                    "demo/doc/doc.pos.xml": POS_XML,
-                    "demo/demo.meta_license.xml": META_XML,
-                    "demo/doc/doc.dep.xml": REL_XML,
-                    "demo/paula_feat.dtd": "ignored",
-                },
-            )
-
+            self._write_archive(root, "demo", "demo", {
+                "demo/doc/doc.text.xml": TEXT_XML,
+                "demo/doc/doc.tok.xml": TOK_XML,
+                "demo/doc/doc.pos.xml": POS_XML,
+                "demo/demo.meta_license.xml": META_XML,
+                "demo/doc/doc.dep.xml": REL_XML,
+                "demo/paula_feat.dtd": "ignored",
+            })
             report = self.audit.audit_upstream(root)
 
         self.assertEqual(report["source_package_count"], 1)
         self.assertEqual(report["parsed_package_count"], 1)
         self.assertEqual(report["packaging"], {"archive": 1})
         self.assertEqual(report["xml_member_count"], 5)
-        self.assertEqual(
-            report["element_kind_counts"],
-            {"body": 1, "featList": 2, "markList": 1, "relList": 1},
-        )
-        self.assertEqual(
-            report["list_type_occurrences"],
-            {
-                "featList": {"license": 1, "pos": 1},
-                "markList": {"tok": 1},
-                "relList": {"dep": 1},
-            },
-        )
+        self.assertEqual(report["element_kind_counts"], {"body": 1, "featList": 2, "markList": 1, "relList": 1})
+        self.assertEqual(report["list_type_occurrences"], {"featList": {"license": 1, "pos": 1}, "markList": {"tok": 1}, "relList": {"dep": 1}})
         self.assertEqual(report["metadata_feature_type_occurrences"], {"license": 1})
-        self.assertEqual(
-            report["metadata_feature_instances"],
-            [
-                {
-                    "dataset": "demo/demo",
-                    "source": "demo/demo_PAULA.zip!/demo/demo.meta_license.xml",
-                    "paula_id": "demo.meta_license",
-                    "base": "demo.anno.xml",
-                    "type": "license",
-                    "values": ["CC-BY 4.0"],
-                }
-            ],
-        )
+        self.assertEqual(report["metadata_feature_instances"], [{"dataset": "demo/demo", "source": "demo/demo_PAULA.zip!/demo/demo.meta_license.xml", "paula_id": "demo.meta_license", "base": "demo.anno.xml", "type": "license", "values": ["CC-BY 4.0"]}])
         self.assertEqual(report["errors"], [])
+
+    def test_pepper_meta_base_is_metadata(self):
+        with tempfile.TemporaryDirectory() as tmp:
+            root = Path(tmp)
+            self._write_archive(root, "demo", "demo", {"demo/demo.meta_author.xml": PEPPER_META_XML})
+            report = self.audit.audit_upstream(root)
+
+        self.assertEqual(report["metadata_feature_type_occurrences"], {"author": 1})
+        self.assertEqual(report["metadata_feature_instances"][0]["base"], "meta")
+        self.assertEqual(report["metadata_feature_instances"][0]["values"], ["Demo Author"])
 
     def test_annofeat_is_not_metadata_and_multifeat_metadata_is_expanded(self):
         with tempfile.TemporaryDirectory() as tmp:
             root = Path(tmp)
-            self._write_archive(
-                root,
-                "demo",
-                "demo",
-                {
-                    "demo/demo.anno_feat.xml": ANNOFEAT_XML,
-                    "demo/demo.meta_multiFeat.xml": MULTI_META_XML,
-                },
-            )
-
+            self._write_archive(root, "demo", "demo", {
+                "demo/demo.anno_feat.xml": ANNOFEAT_XML,
+                "demo/demo.meta_multiFeat.xml": MULTI_META_XML,
+            })
             report = self.audit.audit_upstream(root)
 
-        self.assertEqual(
-            report["list_type_occurrences"],
-            {
-                "featList": {"annoFeat": 1},
-                "multiFeatList": {"multiFeat": 1},
-            },
-        )
-        self.assertEqual(
-            report["metadata_feature_type_occurrences"],
-            {"language": 1, "source_format": 1},
-        )
-        self.assertEqual(
-            report["metadata_feature_instances"],
-            [
-                {
-                    "dataset": "demo/demo",
-                    "source": "demo/demo_PAULA.zip!/demo/demo.meta_multiFeat.xml",
-                    "paula_id": "demo.meta_multiFeat",
-                    "base": "demo.anno.xml",
-                    "type": "language",
-                    "values": ["Coptic"],
-                },
-                {
-                    "dataset": "demo/demo",
-                    "source": "demo/demo_PAULA.zip!/demo/demo.meta_multiFeat.xml",
-                    "paula_id": "demo.meta_multiFeat",
-                    "base": "demo.anno.xml",
-                    "type": "source_format",
-                    "values": ["PAULA XML"],
-                },
-            ],
-        )
+        self.assertEqual(report["list_type_occurrences"], {"featList": {"annoFeat": 1}, "multiFeatList": {"multiFeat": 1}})
+        self.assertEqual(report["metadata_feature_type_occurrences"], {"language": 1, "source_format": 1})
+        self.assertEqual(report["metadata_feature_instances"], [
+            {"dataset": "demo/demo", "source": "demo/demo_PAULA.zip!/demo/demo.meta_multiFeat.xml", "paula_id": "demo.meta_multiFeat", "base": "demo.anno.xml", "type": "language", "values": ["Coptic"]},
+            {"dataset": "demo/demo", "source": "demo/demo_PAULA.zip!/demo/demo.meta_multiFeat.xml", "paula_id": "demo.meta_multiFeat", "base": "demo.anno.xml", "type": "source_format", "values": ["PAULA XML"]},
+        ])
 
     def test_nested_archive_packaging_is_counted_once(self):
         with tempfile.TemporaryDirectory() as tmp:
             root = Path(tmp)
-            self._write_archive(
-                root,
-                "nested",
-                "nested",
-                {"nested/doc.text.xml": TEXT_XML},
-                nested=True,
-            )
-
+            self._write_archive(root, "nested", "nested", {"nested/doc.text.xml": TEXT_XML}, nested=True)
             report = self.audit.audit_upstream(root)
-
         self.assertEqual(report["source_package_count"], 1)
         self.assertEqual(report["parsed_package_count"], 1)
         self.assertEqual(report["datasets"], ["nested/nested"])
@@ -203,43 +150,20 @@ class PaulaAuditContractTests(unittest.TestCase):
     def test_malformed_xml_is_reported_with_member_identity(self):
         with tempfile.TemporaryDirectory() as tmp:
             root = Path(tmp)
-            self._write_archive(
-                root,
-                "broken",
-                "broken",
-                {
-                    "broken/good.text.xml": TEXT_XML,
-                    "broken/bad.xml": "<paula><broken>",
-                },
-            )
-
+            self._write_archive(root, "broken", "broken", {"broken/good.text.xml": TEXT_XML, "broken/bad.xml": "<paula><broken>"})
             report = self.audit.audit_upstream(root)
-
         self.assertEqual(report["source_package_count"], 1)
         self.assertEqual(report["parsed_package_count"], 1)
         self.assertEqual(len(report["errors"]), 1)
         self.assertEqual(report["errors"][0]["kind"], "malformed_xml")
-        self.assertEqual(
-            report["errors"][0]["source"],
-            "broken/broken_PAULA.zip!/broken/bad.xml",
-        )
+        self.assertEqual(report["errors"][0]["source"], "broken/broken_PAULA.zip!/broken/bad.xml")
 
     def test_report_is_deterministic(self):
         with tempfile.TemporaryDirectory() as tmp:
             root = Path(tmp)
-            self._write_archive(
-                root,
-                "demo",
-                "demo",
-                {
-                    "z/doc.pos.xml": POS_XML,
-                    "a/demo.meta_license.xml": META_XML,
-                    "m/doc.text.xml": TEXT_XML,
-                },
-            )
+            self._write_archive(root, "demo", "demo", {"z/doc.pos.xml": POS_XML, "a/demo.meta_license.xml": META_XML, "m/doc.text.xml": TEXT_XML})
             first = self.audit.audit_upstream(root)
             second = self.audit.audit_upstream(root)
-
         self.assertEqual(first, second)
         self.assertEqual(self.audit.render_report_json(first), self.audit.render_report_json(second))
 
